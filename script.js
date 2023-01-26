0f1cb942df09e42cafae15abd8c84a21f1ab7233
ashreyas431 <113090430+ashreyas431@users.noreply.github.com>
2023-01-27 02:05:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+import Ball from "./Ball";
+import Paddle from "./Paddle";
+
+const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+const playerScoreElement = document.getElementById("player-score");
+const computerScoreElement = document.getElementById("computer-score");
+
+let lastTime;
+
+function update(time) {
+  if (lastTime != null) {
+    const delta = time - lastTime;
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+    computerPaddle.update(delta, ball.y);
+    const hue = parseFloat(
+      getComputedStyle(document.documentElement).getPropertyValue("--hue")
+    );
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
+
+    if (isLose()) {
+      handleLose();
+    }
+  }
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+function isLose() {
+  const rect = ball.rect();
+  return rect.right >= window.innerWidth || rect.top <= 0;
+}
+
+function handleLose() {
+  ball.reset();
+  computerPaddle.reset();
+  if (rect.right >= window.innerWidth)
+    playerScoreElement.textContent =
+      parseInt(playerScoreElement.textContent) + 1;
+  else
+    computerScoreElement.textContent =
+      parseInt(computerScoreElement.textContent) + 1;
+}
+
+document.addEventListener("mousemove", (e) => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
+
+window.requestAnimationFrame(update);
